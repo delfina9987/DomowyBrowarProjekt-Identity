d92cc2e61f9439aecce1e68a9db9345cca492c30
nazwa <bla@bla.pl>
2022-06-16 13:48:04 +0200

    update stron o nas, kontakt, regulamin, polityka prywatności + dodanie strony autorzy

---
+++ WebApplication1/Controllers/AuthorshipController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AuthorshipController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Authorship()
+        {
+            return View();
+        }
+    }
+}
